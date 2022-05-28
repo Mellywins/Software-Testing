0abc1cfd836f8833bb64a561ad88fd31184b099c
Mellywins <zouaghi.wow1919@gmail.com>
2022-05-28 13:43:02 +0100

    using docker DNS to resolve postgres ip

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { TodoModule } from './todo/todo.module';
     TodoModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'postgres',
       port: 5432,
       username: 'oussema.zouaghi',
       password: 'oussema.zouaghi',
